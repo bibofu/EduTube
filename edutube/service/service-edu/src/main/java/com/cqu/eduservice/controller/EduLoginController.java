bd4f8c0c5aaf2fb509557b9a7b84f960b832a340
bibofu <bibofu24@gmail.com>
2021-07-06 14:36:06 +0800

    day2-aliyunoss

--- edutube/service/service-edu/src/main/java/com/cqu/eduservice/controller/EduLoginController.java
+++ edutube/service/service-edu/src/main/java/com/cqu/eduservice/controller/EduLoginController.java
@@ -1,6 +1,8 @@
 package com.cqu.eduservice.controller;
 
 import com.cqu.commonutils.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -8,17 +10,21 @@ import org.springframework.web.bind.annotation.*;
  * @create 2021-07-06 上午10:24
  */
 
+
+@Api(description = "管理员登录")
 @RestController
 @CrossOrigin
 @RequestMapping("/eduservice/user")
 public class EduLoginController {
 
+    @ApiOperation(value = "登录返回token")
     @PostMapping("login")
     public R login(){
 
         return R.ok().data("token","admin");
     }
 
+    @ApiOperation(value = "返回用户信息")
     @GetMapping("info")
     public R info() {
         return R.ok().data("roles", "[admin]").data("name", "admin").data("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
