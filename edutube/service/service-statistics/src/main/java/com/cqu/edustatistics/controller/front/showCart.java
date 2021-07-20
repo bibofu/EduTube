c4e328d8f7b04b9b49fea57e27b06048dd933ccb
bibofu <bibofu24@gmail.com>
2021-07-20 10:40:07 +0800

    7.20 show

--- edutube/service/service-statistics/src/main/java/com/cqu/edustatistics/controller/front/showCart.java
+++ edutube/service/service-statistics/src/main/java/com/cqu/edustatistics/controller/front/showCart.java
@@ -1,10 +1,14 @@
 package com.cqu.edustatistics.controller.front;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cqu.commonutils.R;
+import com.cqu.edustatistics.entity.StatisticsDaily;
 import com.cqu.edustatistics.service.StatisticsDailyService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,4 +28,23 @@ public class showCart {
         Map<String, Object> map = dailyService.getChartData(begin, end, type);
         return R.ok().data(map);
     }
+
+    @ApiOperation(value = "获取统计数据")
+    @GetMapping("showdata/{day}")
+    public R getData(@PathVariable String day){
+
+        QueryWrapper<StatisticsDaily> wrapper=new QueryWrapper<>();
+        wrapper.eq("date_calculated",day);
+        StatisticsDaily daily = dailyService.getOne(wrapper);
+
+        Integer loginNum = daily.getLoginNum();
+        Integer registerNum = daily.getRegisterNum();
+        Integer videoViewNum = daily.getVideoViewNum();
+        Integer courseNum = daily.getCourseNum();
+
+        return R.ok().data("loginNum",loginNum).data("registerNum",registerNum).data("videoNum",videoViewNum).data("courseNum",courseNum);
+
+
+    }
+
 }
